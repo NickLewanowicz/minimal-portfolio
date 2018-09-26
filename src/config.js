86a52f13f74cadd6e2d7458bd160ebd2b008bb11
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-26 17:37:05 -0400

    Initialize config to house all profile data

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const data = {
+  personal: {
+    name: "Nicholas Lewanowicz",
+    email: "nicklewanowicz@gmail.com",
+    title: "Software Engineer",
+    image: "https://avatars1.githubusercontent.com/u/15663269?s=460&v=4"
+  }
+}
+
+export default data
