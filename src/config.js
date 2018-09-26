a24452103089e217bff81884c56d9ad51ddba55b
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-26 19:36:28 -0400

    add sample description to test

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
 const data = {
   personal: {
-    name: "Nicholas Lewanowicz",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
     email: "nicklewanowicz@gmail.com",
+    image: "https://avatars1.githubusercontent.com/u/15663269?s=460&v=4",
+    name: "Nicholas Lewanowicz",
     title: "Software Engineer",
-    image: "https://avatars1.githubusercontent.com/u/15663269?s=460&v=4"
   }
 }
 
