3b7d43331ae366f5bf9cc8f5103db1e894f5b2a9
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-26 20:16:28 -0400

    Use project as li in ul

--- src/pages/Home/components/Projects.tsx
+++ src/pages/Home/components/Projects.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react'
+import Project from './Project';
 
 // import Profile from '../../../config'
 
 
-const Header = () => {
+const Projects = () => {
   return (
     <div className='flex flex-column w-100-l'>
         <p className='f3 b'>Things I worked on:</p>
-        
-          <div className='mw6-ns br3 hidden ba b--black-10 shadow-hover'>
-            <div className='flex flex-column ma2'>
-              <p className='f2 mb1 fw5'>Sample Project.</p>
-              <p className=''>Short pitch on what is does.</p>
-
-            </div>
-          </div>
+        <ul className='list pl0 br3 ba b--black-10 shadow-hover'>
+          <Project/>
+          <Project/>
+          <Project/>
+          <Project/>
+        </ul>
     </div>
   )
 } 
 
-export default Header
+export default Projects
