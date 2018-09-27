7492f07e1f9f51f4b97d32beb21033150128fec5
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-27 18:59:44 -0400

    Adjust tachyons for home to properly scale columns

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,12 +3,12 @@ import {Experience, Header, Projects} from './components'
 
 const Home = () => {
   return (
-    <div className="center flex flex-row-ns flex-column sans-serif">
-      <div className='mh3 justify-center'>
+    <div className="center flex flex-column w-90-m w-70-l flex-column-m flex-row-l sans-serif">
+      <div className='center ph2 mh3 w-100 mw6'>
       <Header/>
       <Projects/>
       </div>
-      <div className='mh3 justify-center'>
+      <div className='center ph2 mh3 w-100 mw6'>
       <Experience/>
       </div>
     </div>
