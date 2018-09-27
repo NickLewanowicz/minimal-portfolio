1fac2171e6308c54606368e3bcfee447d2d6334b
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-26 20:16:15 -0400

    Init project component

---
+++ src/pages/Home/components/Project.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+// import Profile from '../../../config'
+
+const Project = () => {
+  return (
+    <li className='flex flex-column bb pa2  b--black-10 shadow-hover'>
+      <p className='f2 mb1 fw5'>Sample Project.</p>
+      <p className=''>Short pitch on what is does.</p>
+    </li>
+  )
+}
+
+export default Project
