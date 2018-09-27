0df71c4bf236ead161f0f80dbd797660e273758e
NickLewanowicz <nicklewanowicz@gmail.com>
2018-09-27 19:00:21 -0400

    Use react-flip to have fullscreen cards

--- src/pages/Home/components/Project.tsx
+++ src/pages/Home/components/Project.tsx
@@ -1,14 +1,45 @@
 import * as React from 'react'
 
-// import Profile from '../../../config'
+import { Flipped, Flipper } from 'react-flip-toolkit';
 
-const Project = () => {
-  return (
-    <li className='flex flex-column bb pa2  b--black-10 shadow-hover'>
-      <p className='f2 mb1 fw5'>Sample Project.</p>
-      <p className=''>Short pitch on what is does.</p>
-    </li>
-  )
+// import Profile from '../../../config'
+interface IState {
+  fullScreen: boolean;
+}
+
+
+class Project extends React.Component<{}, IState> {
+  public state: IState = { fullScreen: false };
+
+  public toggleFullScreen = () => {
+    this.setState(prevState => ({
+      fullScreen: !prevState.fullScreen
+    }));
+  };
+
+  public render() {
+    return (
+      <Flipper flipKey={this.state.fullScreen}>
+          <Flipped flipId="card">
+
+          <div
+            className={`bg-near-white ${this.state.fullScreen ? "full-screen-card " : "card"}`}
+            onClick={this.toggleFullScreen}
+          >
+          <Flipped flipId="card">
+
+          <div className={`flex flex-column mw6 ${this.state.fullScreen ? 'center mt3' : ' bb pa2  b--black-10 shadow-hover'}`}>
+            <p className='f2 mb1 fw5'>Sample Project.</p>
+            <p className=''>Short pitch on what is does.</p>
+          </div>
+          </Flipped>
+
+          </div>
+          </Flipped>
+
+      </Flipper>
+    );
+  }
 }
 
 export default Project
